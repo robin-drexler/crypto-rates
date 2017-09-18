e2fc15632825d07f7c3a7d66baa0d49733fd0491
Robin Drexler <drexler.robin@gmail.com>
2017-09-18 19:51:24 +0200

    refactor: use actions-on-google lib to generate responses

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const ApiAiApp = require('actions-on-google').ApiAiApp;
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res) => {
-  const send = responseString => {
-    res.json({
-      speech: responseString,
-      displayText: responseString
-    });
-  };
+/**
+ * 
+ * @param {ApiAiApp} app 
+ */
+const askRateIntent = app => {
   const supportedCurrencies = {
     bitcoin: 'BTC',
     ethereum: 'ETH',
     iota: 'IOT'
   };
 
-  const currency = req.body.result.parameters['crypto-currency'];
+  const currency = app.getArgument('crypto-currency');
 
   if (!currency) {
-    return send("Sorry, I don't understand that yet");
+    return app.tell("Sorry, I don't understand that yet");
   }
 
   const symbol = supportedCurrencies[currency];
   if (!symbol) {
-    send(
+    return app.tell(
       `Sorry, I can't tell the exchange rate for ${currency} yet. But, I'm learning every day.`
     );
   }
@@ -35,10 +35,18 @@ app.use('/', (req, res) => {
   fetch(`https://api.bitfinex.com/v2/ticker/t${symbol}USD`)
     .then(r => r.json())
     .then(json => {
-      send(
+      return app.tell(
         `The current price for ${currency} on bitfinex.com is $${json[6].toFixed(2)}`
       );
     });
+};
+
+app.use('/', (req, res) => {
+  const app = new ApiAiApp({ request: req, response: res });
+  const actionMap = new Map();
+
+  actionMap.set('ask.rate', askRateIntent);
+  app.handleRequest(actionMap);
 });
 
 app.listen(process.env.PORT || 3000);
