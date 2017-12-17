ab0c8779b4a16b3f9ca27789505aaa3dfbf8f198
Robin Drexler <drexler.robin@gmail.com>
2017-12-17 15:13:10 +0100

    upgrade dependencies, use coinmarketcap api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const ApiAiApp = require('actions-on-google').ApiAiApp;
+const DialogflowApp = require('actions-on-google').DialogflowApp;
 
 const app = express();
 
@@ -9,39 +9,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 /**
- * 
- * @param {ApiAiApp} app 
+ *
+ * @param {ApiAiApp} app
  */
 const hasScreen = app =>
   app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
 
+// example for screen cap
+// if (hasScreen(app)) {
+//   return app.ask(
+//     app
+//       .buildRichResponse()
+//       .addSimpleResponse(
+//         `Sorry, I can't tell the exchange rate for ${currency} yet. Would you like to check on bitfinex.com?`
+//       )
+//       .addSuggestionLink('bitfinex.com', 'https://www.bitfinex.com/')
+//   );
+// }
+
 /**
- * 
- * @param {ApiAiApp} app 
+ *
+ * @param {ApiAiApp} app
  */
 const askRateIntent = app => {
-  const handleUnsupportedCurrency = currency => {
-    if (hasScreen(app)) {
-      return app.ask(
-        app
-          .buildRichResponse()
-          .addSimpleResponse(
-            `Sorry, I can't tell the exchange rate for ${currency} yet. Would you like to check on bitfinex.com?`
-          )
-          .addSuggestionLink('bitfinex.com', 'https://www.bitfinex.com/')
-      );
-    }
-    return app.tell(
-      `Sorry, I can't tell the exchange rate for ${currency} yet.`
-    );
-  };
-
-  const supportedCurrencies = {
-    bitcoin: 'BTC',
-    ethereum: 'ETH',
-    iota: 'IOT',
-    litecoin: 'LTC'
-  };
+  const supportedCurrencies = ['bitcoin', 'ethereum', 'iota', 'litecoin'];
 
   const currency = app.getArgument('crypto-currency');
 
@@ -49,22 +40,25 @@ const askRateIntent = app => {
     return app.tell("Sorry, I don't understand that yet");
   }
 
-  const symbol = supportedCurrencies[currency];
-  if (!symbol) {
-    return handleUnsupportedCurrency(currency);
+  if (!supportedCurrencies.includes(currency)) {
+    return app.tell(
+      `Sorry, I can't tell the exchange rate for ${currency} yet.`
+    );
   }
 
-  fetch(`https://api.bitfinex.com/v2/ticker/t${symbol}USD`)
+  fetch(`https://api.coinmarketcap.com/v1/ticker/${currency}`)
     .then(r => r.json())
-    .then(json => {
+    .then(([json]) => {
       return app.tell(
-        `The current price for ${currency} on bitfinex.com is $${json[6].toFixed(2)}`
+        `The current price for ${
+          json.name
+        } on coinmarketcap.com is $${parseFloat(json.price_usd, 10).toFixed(2)}`
       );
     });
 };
 
 app.use('/', (req, res) => {
-  const app = new ApiAiApp({ request: req, response: res });
+  const app = new DialogflowApp({ request: req, response: res });
   const actionMap = new Map();
 
   actionMap.set('ask.rate', askRateIntent);
